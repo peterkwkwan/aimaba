8d55645d0482746037c2e1246c951b1a166b5438
peterkwkwan <peterkwkwan@gmail.com>
2024-02-04 09:16:57 +0800

    feat: blog set up

--- sanity.config.ts
+++ sanity.config.ts
@@ -21,8 +21,7 @@ import authorType from 'schemas/author'
 import postType from 'schemas/post'
 import settingsType from 'schemas/settings'
 
-const title =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || 'Next.js Blog with Sanity.io'
+const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || 'Aimaba Blog'
 
 export default defineConfig({
   basePath: '/studio',
